53b8e0a4d0ec7d8da0bfcc2c709016e3d8ed470f
SohamShah20 <sohamjshah20@gmail.com>
2025-07-29 10:51:16 +0530

    Forgot Password change
    Can use forgot password as dealer and customer seperately

--- client/src/pages/ForgetPassword.jsx
+++ client/src/pages/ForgetPassword.jsx
@@ -6,12 +6,13 @@ import axios from 'axios'
 
 function ForgotPassword() {
     const [email, setEmail] = useState()
+    const [isDealer, setIsDealer] = useState(false)
     const navigate = useNavigate()
 
     axios.defaults.withCredentials = true;
     const handleSubmit = (e) => {
         e.preventDefault()
-        axios.post('http://localhost:3001/forgot-password', {email})
+        axios.post('http://localhost:3001/forgot-password', {email ,isDealer})
         .then(res => {
             if(res.data.Status === "Success") {
                 navigate('/login')
@@ -37,6 +38,17 @@ function ForgotPassword() {
               required
               className="form-control rounded-0"
               onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="dealer">
+              Are you a dealer?
+            </label>
+            <input 
+              type="checkbox"
+              name="dealer"
+              checked={isDealer}
+              onChange={(e) => setIsDealer(e.target.checked)}
             />
           </div>
           <button type="submit" className="btn btn-success w-100 rounded-0">
